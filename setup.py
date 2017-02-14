3bb5705c6ce92fc27f66dd9fe6e2593fa6069a65
Alexandre Kandalintsev <exe.sre@gmail.com>
2017-02-14 13:44:19 +0100

    setup.py: initial version

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pip-token.py',
+      version='0.1',
+      description='Wrapper for pip that makes github token auth possible',
+      url='https://github.com/alex-cg1/pip-token.py',
+      author='MailExpress Squad',
+      author_email='tech@customergauge.com',
+      license='WTFPL',
+      scripts=['pip-token.py']
+      )
